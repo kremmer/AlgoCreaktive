3adfff9c95f39e4b82046133d50d9b54ae4639e9
kremmer <eric.kremmer@epsi.fr>
2015-01-27 13:46:21 +0100

    lib

--- api.js
+++ api.js
@@ -1,1 +1,13 @@
 
+GetAllFeuilles : function(father)                           // Cette fonction vas réutiliser en boucle
+{                                                           // la fonction GetFeuilles.
+  var TabFeuilles =[];                                      // Cela nous permet de faire un tableau des
+  var TabDChilds = lib.GetChilds(father);                   // de TOUS les elements et donc de savoir
+  // qui est la feuille, qui se situe à la
+  for ( var f = 0; TabDChilds.length > 0; f++)             // fin de la branche.
+  {                                                         // Elle retourne notre tableau contenant
+    TabDChilds = lib.GetFeuilles(TabDChilds, TabFeuilles)   // toutes les feuilles.
+  }
+
+  return TabFeuilles;
+},
